eefccfcaa8f7f2ae3ad3c5bfcbd71e22e122c057
Cody Rains <cody.rains34@gmail.com>
2021-07-14 16:41:56 -0400

    Updated Nav bar and medium/large screen css

--- js/site.js
+++ js/site.js
@@ -1,1 +1,9 @@
 //alert("Hello, this is from my js file!");
+
+// menu toggle
+
+function toggleMenu() {
+    document.getElementById('primaryNav').classList.toggle("open");
+}
+let x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
